e2105c9a66c221eca545e8e2e413cf4c697218b3
Satrio Adi Prawiro <prawiro.96@gmail.com>
2022-12-24 09:44:49 +0700

    update screenshot and decription all testcase in resgister web

--- Test Suites/Web/TS_Register.ts
+++ Test Suites/Web/TS_Register.ts
@@ -15,84 +15,84 @@
       <guid>d83f616d-3cf6-4163-8304-4ccec0c57225</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-001</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-001 - Go to Register Page (Webapp)</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>748e0748-14d9-4325-84af-99492151bc13</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-002</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-002 - Go to Register page (webapp, through Login Page)</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>ed3ae36d-48b3-4c15-9576-a4265a1f8683</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-003</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-003 - Go to Register Page (Website)</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>2176209e-44f6-44aa-ba16-c65806a7a363</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-004</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-004 - To Register and Back to Home</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>b6fa0929-2cea-4f32-8622-52763df0f735</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-005</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-005 - To Register, then Home and Back to Register</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>956b7c66-0ded-490c-af8f-14573cb723f6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-006</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-006 - Go to Register (website, through Login)</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>0cd3360f-6798-4fbb-9f57-cbb531351b23</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-007</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-007 - To Register with SiteMap in the footer of Homepage</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>c69531c2-69a6-4c27-ab49-0fb96d00b6df</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-008</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-008 - To Register through Sitemap in the footer of Homepage (login)</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>94527ffb-4c3b-439f-b3c6-64f983997932</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-009</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-009 - Register Button in Register Page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>d875a8aa-e393-46ff-b12e-babe0eda99a8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-010</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-010 - To Register through Sitemap in footer of Register Page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>1d89e1e1-5ef7-46b2-a4ef-48ff5cfc938b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-011</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-011 - To Register through Sitemap in footer of Register Page (Login)</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>92710893-be19-408c-abef-a405cf02f5a7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web/Register/RG-012</testCaseId>
+      <testCaseId>Test Cases/Web/Register/RG-012 - Input Empty data to register</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
