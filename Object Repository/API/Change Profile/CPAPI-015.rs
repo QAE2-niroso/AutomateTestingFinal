f146eb91f94eb054e423c0f9a195907e1a558f22
Rifqi Fajar Ramdani <rifqifajar24@gmail.com>
2023-01-03 12:29:28 +0700

    API Chanage Profile version 1

---
+++ Object Repository/API/Change Profile/CPAPI-015.rs
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>CPAPI-015</name>
+   <tag></tag>
+   <elementGuidId>0e66b723-4802-42a3-8391-e283e4e0f0a8</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <authorizationRequest>
+      <authorizationInfo>
+         <entry>
+            <key>bearerToken</key>
+            <value>eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiNWE5YjFiMjRmNDA5NjA4MDFmNjg2OWQ3NGYxNTA4MWI4MjhlYjhjODA4YWIyN2ZmYWI5Y2FhMDk4OWRjZWM5ZWI5ODJjODQyZWQ4ZGFkOWUiLCJpYXQiOjE2NzI3MjA2MzguOTY0NDYsIm5iZiI6MTY3MjcyMDYzOC45NjQ0NjQsImV4cCI6MTcwNDI1NjYzOC45NjEwMiwic3ViIjoiNjQiLCJzY29wZXMiOltdfQ.bflVWoGV-PLWQboxQU6IK2ka3TFcNmLIbtL4x-JVR_10dOxkK5FkdV2VhFwpGnAXn8H_qFX6-k9uEpC6JV5G8E8SGt1Zv55-sNPw8NXDXtxNjGX1C_uAxtqLyJm3Hj95czdMwMLstrqLRN1CIyjECl881EFAKYiTdf2hv0VqkqjaZnLrKQ2QpE8qcJX6TsFjCZuHsixLPKwaYFMfJDljvRRP2dzLAyE1wKkaY4zFbwwr1Ua0Cqvhz-AcKahIsrr5uoGIv2aI9qhQwxsFsrsC9AlHXH8SX0nvTNeiSemIu77R2IflDgc6_XeHgvBBxpS5q6jpqe2CAHxfFZr4FU56O_BzwAGXrERwMaf_0VWd_IXTumqwMh0Wk7-C0UGRhLM5TPR2LrVRs8bsFRuR1dkuS6YptIYPn6f0WftkVXYGLSzb1VHY7nGjagyhpVHliloGvLYENjJKBzZWdFlzy3cDE1amh2ykJ4wAMrMW41eXTjnkOs1PCTgBbB1of9OWa9cLV3mVFHcoFUrOEaZJoLxHOAuWmoO9uKx57xIgP2ov3kLX-PmRCTKGC19P0YA4lqM-e4Ah-5JYhJGq3_aTwn4aLAOJ7TIFyO9bbOF01bIyBJpxExCkpy2BuBtgQunsg97oNadhbFM2lPxzT09gK9u55x--VBDwqNi2-POZClHPFqc</value>
+         </entry>
+      </authorizationInfo>
+      <authorizationType>Bearer</authorizationType>
+   </authorizationRequest>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n  \&quot;name\&quot;: \&quot;Rifqi\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>multipart/form-data</value>
+      <webElementGuid>162d0b95-e432-422d-99e8-45c56ccb24f6</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiNWE5YjFiMjRmNDA5NjA4MDFmNjg2OWQ3NGYxNTA4MWI4MjhlYjhjODA4YWIyN2ZmYWI5Y2FhMDk4OWRjZWM5ZWI5ODJjODQyZWQ4ZGFkOWUiLCJpYXQiOjE2NzI3MjA2MzguOTY0NDYsIm5iZiI6MTY3MjcyMDYzOC45NjQ0NjQsImV4cCI6MTcwNDI1NjYzOC45NjEwMiwic3ViIjoiNjQiLCJzY29wZXMiOltdfQ.bflVWoGV-PLWQboxQU6IK2ka3TFcNmLIbtL4x-JVR_10dOxkK5FkdV2VhFwpGnAXn8H_qFX6-k9uEpC6JV5G8E8SGt1Zv55-sNPw8NXDXtxNjGX1C_uAxtqLyJm3Hj95czdMwMLstrqLRN1CIyjECl881EFAKYiTdf2hv0VqkqjaZnLrKQ2QpE8qcJX6TsFjCZuHsixLPKwaYFMfJDljvRRP2dzLAyE1wKkaY4zFbwwr1Ua0Cqvhz-AcKahIsrr5uoGIv2aI9qhQwxsFsrsC9AlHXH8SX0nvTNeiSemIu77R2IflDgc6_XeHgvBBxpS5q6jpqe2CAHxfFZr4FU56O_BzwAGXrERwMaf_0VWd_IXTumqwMh0Wk7-C0UGRhLM5TPR2LrVRs8bsFRuR1dkuS6YptIYPn6f0WftkVXYGLSzb1VHY7nGjagyhpVHliloGvLYENjJKBzZWdFlzy3cDE1amh2ykJ4wAMrMW41eXTjnkOs1PCTgBbB1of9OWa9cLV3mVFHcoFUrOEaZJoLxHOAuWmoO9uKx57xIgP2ov3kLX-PmRCTKGC19P0YA4lqM-e4Ah-5JYhJGq3_aTwn4aLAOJ7TIFyO9bbOF01bIyBJpxExCkpy2BuBtgQunsg97oNadhbFM2lPxzT09gK9u55x--VBDwqNi2-POZClHPFqc</value>
+      <webElementGuid>79e5d502-e3d3-44e8-945c-0387d7ba3caa</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>8.5.2</katalonVersion>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>https://demo-app.online/api/updateprofile</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+WS.verifyElementPropertyValue(response, 'name',&quot;Rifqi&quot;)
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Object Repository/API/Change Profile/CPAPI-017.rs
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>CPAPI-017</name>
+   <tag></tag>
+   <elementGuidId>840042c4-dc55-4bd5-8f08-eedb03124c7c</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>8.5.2</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>https://demo-app.online/api/updateprofile</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
